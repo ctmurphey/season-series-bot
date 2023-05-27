38322560e722099fd7df0de522b6e2e5ca53de5d
Chauncey Tanner Murphey <ctmurphey@protonmail.com>
2023-05-27 16:01:38 -0400

    updated readme, reddit comment

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # season-series-bot
 A bot for Reddit that automatically posts the season series status of a team over a certain time frame (like at the end of each game day). The non-bot version of this code (and prototype upgrades) can be found in the `mets-season-series` folder in [my baseball repo](https://github.com/ctmurphey/baseball). 
+
+### Planned upgrades:
+1. Automating bot to wait until right time to post figure as opposed to once the code is running
+    1. Will require constant running/deployment
+2. Get to run constantly and only update at appropriate time
+    1. Allow time frame to vary (after games/series, daily, weekly, etc.)
+    2. Will require database setup
+3. Allow handling of multiple teams with potentially unique times for posting
+
+### Non-deployed upgrade plans:
+1. Make backend in Julia
+2. Use pie charts instead of stacked bar charts
--- bot.py
+++ bot.py
@@ -18,8 +18,8 @@ reddit = praw.Reddit(
     password=pwd
 )
 today = date.today()
-# subreddit = reddit.subreddit("NewYorkMets") #uncomment when deploying
-subreddit = reddit.subreddit("PythonSandlot") # for testing/debugging
+subreddit = reddit.subreddit("NewYorkMets") #uncomment when deploying
+# subreddit = reddit.subreddit("PythonSandlot") # for testing/debugging
 
 print(subreddit.display_name)
 print(reddit.user.me())
@@ -27,9 +27,13 @@ title = "Mets Current Season Series Progress: "+str(today)
 my_post = subreddit.submit_image(title=title, image_path='test.jpg')
 
 # comment_str = "If you see any issues, please message u/just-an-astronomer"
-comment_str = '''^beep ^boop ^I'm ^a ^bot Hello, I'm u/season-series-bot. I'll be taking over the season series graphics from now on.
+comment_str = '''🤖^beep ^boop ^I'm ^a ^bot Hello, I'm u/season-series-bot. I'll 
+be taking over the season series graphics from now on. The account change from 
+human to bot is so that I can be further automated while keeping the personal 
+account more safe/separate. Any prototype upgrades will be tested in r/PythonSandlot 
+before being deployed here.
 
-If you notice any issues, please contact u/just-an-astronomer.
+If you do notice any issues, please contact u/just-an-astronomer.
 
-[Github Repo](https://github.com/ctmurphey/season-series-bot) [Creator's Webpage](https://ctmurphey.github.io)'''
+[My Github Repo](https://github.com/ctmurphey/season-series-bot)'''
 my_post.reply(comment_str)
