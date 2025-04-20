bfa4bbb06fffb47983fda180a34c970ec1c8eca8
Chauncey Murphey <94994713+ctmurphey@users.noreply.github.com>
2025-04-20 16:03:46 -0500

    Added support for flairs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # season-series-bot
-A bot that creates a jpg image of stacked bar graph of an MLB team using `MLB-StatsAPI`, `pandas`, and `matplotlib`. Currently set to the New York Mets but can be tweaked to cover any MLB team. To use the bot functionality, you must provide your own bot credentials and store them in a file called `credentials.txt`.
+A bot that creates a jpg image of stacked bar graph of an MLB team using `MLB-StatsAPI`, `pandas`, and `matplotlib`. Currently set to the New York Mets but can be tweaked to cover any MLB team. To use the bot functionality, you must provide your own bot credentials and store them in a file called `credentials.txt`. The file also assumes the credentials file has a flair id (since r/NewYorkMets requires flairs upon submission). People using this need to either to find the correct flair ID ([which I found here](https://www.reddit.com/r/redditdev/comments/ovte4q/praw_flair_a_post/)) or to remove references to the flair in the code.
 
 Example:
 ![Stacked bar graph for the New York Mets](test.jpg)
--- bot.py
+++ bot.py
@@ -10,6 +10,7 @@ client_id = f.readline().split()[1]
 secret = f.readline().split()[1]
 username = f.readline().split()[1]
 pwd = f.readline().split()[1]
+flair = f.readline().split()[1]
 
 reddit = praw.Reddit(
     client_id=client_id,
@@ -19,13 +20,13 @@ reddit = praw.Reddit(
     password=pwd
 )
 today = date.today()
-# subreddit = reddit.subreddit("NewYorkMets") #uncomment when deploying
-subreddit = reddit.subreddit("PythonSandlot") # for testing/debugging
+subreddit = reddit.subreddit("NewYorkMets") #uncomment when deploying
+# subreddit = reddit.subreddit("testingground4bots") # for testing/debugging
 
 print(subreddit.display_name)
 print(reddit.user.me())
 title = "Mets Current Season Series Progress: "+str(today)
-my_post = subreddit.submit_image(title=title, image_path='test.jpg')
+my_post = subreddit.submit_image(title=title, image_path='test.jpg', flair_id=flair)
 print("Image posted")
 # comment_str = "If you see any issues, please message u/just-an-astronomer"
 comment_str = '''beep ^boop beep ^I'm ^a ^bot Hello, I'm u/season-series-bot. I
